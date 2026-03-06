f0c3d2f55876c0c6d703ba611a111c5a98a0fd13
weezeltggv <thijs.vanweezel@tno.nl>
2026-03-06 13:11:34 +0000

    implement resnet equivalent to https://github.com/thijs-vanweezel/loss-symmetries-fl/blob/master/models.py#L255 w/o asym

--- model.py
+++ model.py
@@ -98,3 +98,92 @@ class ModerateCNNContainer(nn.Module):
         x = self.conv_layer(x)
         x = x.view(x.size(0), -1)
         return x
+
+
+class ResNetBlock(nn.Module):
+    def __init__(self, in_kernels: int, out_kernels: int, stride: int = 1):
+        super().__init__()
+        self.stride = stride
+        self.subsample = stride > 1 or in_kernels != out_kernels
+
+        self.conv1 = nn.Conv2d(
+            in_kernels, out_kernels,
+            kernel_size=3, stride=stride, padding=1, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(out_kernels)
+
+        self.conv2 = nn.Conv2d(
+            out_kernels, out_kernels,
+            kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(out_kernels)
+
+        if self.subsample:
+            self.id_conv = nn.Conv2d(
+                in_kernels, out_kernels,
+                kernel_size=1, stride=stride, bias=False
+            )
+
+        self._init_weights()
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        h = F.relu(self.bn1(self.conv1(x)))
+        h = self.bn2(self.conv2(h))
+
+        res = self.id_conv(x) if self.subsample else x
+
+        return F.relu(res + h)
+
+
+class ResNet(nn.Module):
+    def __init__(
+        self,
+        layers: tuple[int, ...] = (3, 4, 6, 3),
+        kernels: tuple[int, ...] = (64, 128, 256, 512),
+        channels_in: int = 3,
+        dim_out: int = 1000,
+    ):
+        super().__init__()
+        assert len(layers) == len(kernels)
+
+        self.conv = nn.Conv2d(
+            channels_in, kernels[0],
+            kernel_size=7, stride=2, padding=3, bias=False
+        )
+        self.bn = nn.BatchNorm2d(kernels[0])
+
+        blocks = []
+        for j, num_blocks in enumerate(layers):
+            for i in range(num_blocks):
+                k_in  = ([kernels[0]] + list(kernels))[j] if i == 0 else kernels[j]
+                k_out = kernels[j]
+                stride = 2 if i == 0 and j > 0 else 1
+                blocks.append(ResNetBlock(k_in, k_out, stride=stride))
+        self.layers = nn.Sequential(*blocks)
+
+        self.fc = nn.Linear(kernels[-1], dim_out)
+
+        self._init_head()
+
+    def _init_head(self):
+        nn.init.kaiming_normal_(self.conv.weight, mode="fan_out", nonlinearity="relu")
+        nn.init.ones_(self.bn.weight)
+        nn.init.zeros_(self.bn.bias)
+        nn.init.kaiming_normal_(self.fc.weight, mode="fan_out", nonlinearity="relu")
+        nn.init.zeros_(self.fc.bias)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = F.relu(self.bn(self.conv(x)))
+        x = F.max_pool2d(x, kernel_size=3, stride=2, padding=1)
+        x = self.layers(x)
+        x = x.mean(dim=(2, 3))
+        x = self.fc(x)
+        return x
