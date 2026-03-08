02b92be46ed188816736a41eda1ee05e472f348c
weezeltggv <thijs.vanweezel@tno.nl>
2026-03-08 11:17:38 +0000

    copy imagenet dataset

--- datasets.py
+++ datasets.py
@@ -1,7 +1,10 @@
+import torch, torchvision, shutil
 import torch.utils.data as data
-from PIL import Image
 import numpy as np
 from torchvision.datasets import CIFAR10
+from torchvision.transforms import v2
+from torchvision.transforms.v2 import functional
+from tqdm.auto import tqdm
 
 import os
 import os.path
@@ -55,3 +58,107 @@ class CIFAR10_truncated(data.Dataset):
 
     def __len__(self):
         return len(self.data)
+
+def train_aug(img:torch.Tensor, seed, mask:torch.Tensor=None):
+    """Deterministic train augmentations"""
+    # Flip
+    if torch.randint(0, 2, (), generator=seed).item():
+        img = img.flip((-1,))
+        if mask is not None: mask = mask.flip((-1,))
+    # Crop
+    i = torch.randint(0, img.shape[1]-224+1, (), generator=seed).item()
+    j = torch.randint(0, img.shape[2]-224+1, (), generator=seed).item()
+    img = functional.crop(img, i, j, 224, 224)
+    if mask is not None: mask = functional.crop(mask, i, j, 224, 224)
+    # Color jitter
+    brightness = 0.8 + torch.rand((), generator=seed).item()*0.4
+    img *= brightness
+    contrast = 0.8 + torch.rand((), generator=seed).item()*0.4
+    mean = torch.mean(img, (1,2), keepdims=True)
+    img = (img - mean) * contrast + mean
+    return img, mask
+
+class ImageNet(data.Dataset):
+    def __init__(self, path:str="/data/bucket/traincombmodels/imnetproc", partition:str="train", n_clients:int=4, 
+                 n_classes:int=1000, originalpath:str="/data/bucket/traincombmodels/imagenet"):
+        # Set random seed. Wait for global seed to be set, so that each worker gets different seed.
+        self.diff_seed = None 
+        self.same_seed = torch.Generator()
+        self.same_seed.manual_seed(42)
+        # Augmentations
+        self.val_crop = v2.CenterCrop(224)
+        self.normalize = v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], inplace=True)
+        # Create split
+        if not os.path.exists(path):
+            self.repartition(originalpath, path)
+        self.partition = partition
+        # Get class names and limit to n_classes by skipping
+        with open(os.path.join(path, "mapping.txt")) as f:
+            classes = f.readlines()
+        classes = classes[::len(classes)//n_classes]
+        classes = {i:line.split()[0] for i, line in enumerate(classes)}
+        # Assign sample paths to each client
+        self.data = {c: [] for c in range(n_clients)}
+        client = 0
+        for class_idx in range(n_classes):
+            classname = classes[class_idx]
+            # Only n_classes
+            if classname in classes.values():
+                dirname = os.path.join(path, partition, classname)
+                filelist = os.listdir(dirname)
+                self.data[client].extend([
+                    (class_idx, os.path.join(dirname, file)) for file in filelist
+                ])
+                client = (client+1) % n_clients
+        self.prev_idx = float("inf")
+        # Misc attributes
+        self.n_clients = n_clients
+        self.n_classes = n_classes
+
+    @staticmethod
+    def repartition(originalpath, newpath, train_frac=0.8):
+        """Divides the train partition into train/val/test"""
+        os.makedirs(os.path.join(newpath, "train"))
+        os.makedirs(os.path.join(newpath, "val"))
+        os.makedirs(os.path.join(newpath, "test"))
+        for classname in tqdm(os.listdir(os.path.join(originalpath, "Data", "CLS-LOC", "train")), leave=False):
+            os.mkdir(os.path.join(newpath, "train", classname))
+            os.mkdir(os.path.join(newpath, "val", classname))
+            os.mkdir(os.path.join(newpath, "test", classname))
+            files = os.listdir(os.path.join(originalpath, "Data", "CLS-LOC", "train", classname))
+            for i, file in enumerate(files):
+                partition = "train" if i<len(files)*train_frac else \
+                "val" if i<len(files)*(train_frac+(1-train_frac)/2) else "test"
+                shutil.copy(os.path.join(originalpath, "Data", "CLS-LOC", "train", classname, file), os.path.join(newpath, partition, classname))
+    
+    def __len__(self):
+        # Take minimum of client lengths (many papers focus on quantity imbalance, which we do not address)
+        return min([len(files) for files in self.data.values()])*self.n_clients
+    
+    def __getitem__(self, idx):
+        # Initialize seed with global seed
+        if self.diff_seed is None:
+            self.diff_seed = torch.Generator()
+            self.diff_seed.manual_seed(torch.randint(0, int(1e6), ()).item())
+        # Shuffle per client at each epoch
+        if idx<self.prev_idx:
+            for c in self.data.keys():
+                sorter = iter(torch.randperm(len(self.data[c]), generator=self.same_seed).tolist())
+                self.data[c].sort(key=lambda _: next(sorter))
+        self.prev_idx = idx
+        # Load datum
+        c = idx % self.n_clients
+        i = idx // self.n_clients
+        label, img_path = self.data[c][i]
+        with open(img_path, "rb") as f:
+            bytesdata = torch.frombuffer(f.read(), dtype=torch.uint8)
+        img = torchvision.io.decode_image(bytesdata, mode="RGB").float() / 255.
+        # Apply augmentations
+        img = functional.resize(img, 256)
+        if self.partition=="train": 
+            img, _ = train_aug(img, seed=self.diff_seed)
+        else: img = self.val_crop(img)
+        self.normalize(img)
+        # Change to HWC
+        img = torch.permute(img, (1,2,0))
+        return label, img
